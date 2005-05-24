e958aae386ad21988be70a221b805d9ae84cbaf7
Nicolas Cannasse <ncannasse@gmail.com>
2005-05-24 08:28:53 +0000

    added exceptions.

--- vm/vm.h
+++ vm/vm.h
@@ -5,6 +5,7 @@
 /*																			*/
 /* ************************************************************************ */
 #pragma once
+#include <setjmp.h>
 #include "neko.h"
 #include "context.h"
 
@@ -16,14 +17,21 @@ struct _neko_vm {
 	int *spmax;
 	int *sp;
 	int *csp;
+	int *trap;
+	objtable fields;
+	value env;
 	value val_this;
 	const char *error;
 	printer print;
+	jmp_buf start;
 	char tmp[100];
 };
 
+extern int *callback_return;
 extern _context *vm_context;
 
 #define NEKO_VM()	((neko_vm*)context_get(vm_context))
+
+extern value interp( neko_vm *vm, int acc, int *pc, value env );
 
 /* ************************************************************************ */
