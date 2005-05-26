88bff2c6c9c19b04273f1054671e00e87bde3acd
Nicolas Cannasse <ncannasse@gmail.com>
2005-05-26 09:44:02 +0000

    added mutual rec functions. added recursive and predeclared types. added list and records declarations.

--- libs/include/ocaml/ml/mltyper.ml
+++ libs/include/ocaml/ml/mltyper.ml
@@ -3,26 +3,28 @@ open Mltype
 
 type context = {
 	mutable idents : (string,t) PMap.t;
+	mutable functions : (texpr ref * t * (string * t) list * expr * t * pos) list;
 	gen : id_gen;
 	records : (string,t * t * mutflag) Hashtbl.t;
 	constr : (string,t * t) Hashtbl.t;
 	types : (string,t) Hashtbl.t;
+	tmptypes : (string, t * t list * (string,t) Hashtbl.t) Hashtbl.t;
 }
 
 type error_msg =
-	| Invalid_expression 
 	| Cannot_unify of t * t
 	| Have_no_field of t * string
 	| Stack of error_msg * error_msg
+	| Unknown_field of string
 	| Custom of string
 
 exception Error of error_msg * pos
 
 let rec error_msg ?(h=s_context()) = function
-	| Invalid_expression -> "Invalid expression"
 	| Cannot_unify (t1,t2) -> "Cannot unify " ^ s_type ~h t1 ^ " and " ^ s_type ~h t2
 	| Have_no_field (t,f) -> s_type ~h t ^ " have no field " ^ f
 	| Stack (m1,m2) -> error_msg ~h m1 ^ "\n  " ^ error_msg ~h m2
+	| Unknown_field s -> "Unknown field " ^ s
 	| Custom s -> s
 
 let error m p = raise (Error (m,p))
@@ -35,8 +37,8 @@ let link ctx t1 t2 p =
 	end else
 		if t2.tid = -1 then t1.tid <- -1
 
-let record_field ctx f =
-	let rt , ft , mut = Hashtbl.find ctx.records f in
+let record_field ctx f p =
+	let rt , ft , mut = (try Hashtbl.find ctx.records f with Not_found -> error (Unknown_field f) p) in
 	let h = Hashtbl.create 0 in
 	duplicate ctx.gen ~h rt, duplicate ctx.gen ~h ft, mut
 
@@ -161,11 +163,6 @@ let addable str e =
 let type_binop ctx op e1 e2 p =
 	let emk t = mk (TBinop (op,e1,e2)) t p in
 	match op with
-	| ":=" ->
-		let t , pt = t_poly ctx.gen "ref" in
-		unify ctx e2.etype pt (pos e2);
-		unify ctx e1.etype t (pos e1);
-		emk t_void
 	| "%"
 	| "+"
 	| "-"
@@ -220,24 +217,86 @@ let type_binop ctx op e1 e2 p =
 	| "!=" ->
 		unify ctx e2.etype e1.etype (pos e2);
 		emk t_bool
+	| ":=" ->
+		(match e1.edecl with
+		| TArray _ ->
+			unify ctx e2.etype e1.etype (pos e2);
+			emk t_void
+		| TField (e,f) ->
+			(match tlinks false e.etype with
+			| TRecord fl ->
+				let _ , mut , _ = (try List.find (fun (f2,_,_) -> f2 = f) fl with Not_found -> assert false) in
+				if mut = Immutable then error (Custom ("Field " ^ f ^ " is not mutable")) (pos e1);
+				unify ctx e2.etype e1.etype (pos e2);
+				emk t_void
+			| _ -> assert false);
+		| _ ->
+			let t , pt = t_poly ctx.gen "ref" in
+			unify ctx e2.etype pt (pos e2);
+			unify ctx e1.etype t (pos e1);
+			emk t_void)
+	| "::" ->
+		let t , pt = t_poly ctx.gen "list" in
+		unify ctx e1.etype pt (pos e1);
+		unify ctx e2.etype t (pos e2);
+		emk t
 	| _ ->
 		error (Custom ("Invalid operation " ^ op)) p
 
-let rec type_expr ctx (e,p) =
+let register_function ctx name pl e rt p =
+	let pl = (match pl with [] -> ["_",Some (EType ([],"void"))] | _ -> pl) in
+	let expr = ref (mk (TConst TVoid) t_void p) in
+	let h = Hashtbl.create 0 in
+	let el = List.map (fun (pn,pt) ->
+		let t = (match pt with
+			| None -> t_mono()
+			| Some pt -> type_type ~h ctx pt p
+		) in
+		pn , t
+	) pl in
+	let rt = (match rt with 
+		| None -> t_mono() 
+		| Some rt -> type_type ~h ctx rt p
+	) in
+	let ft = mk_fun ctx.gen (List.map snd el) rt in
+	ctx.functions <- (expr,ft,el,e,rt,p) :: ctx.functions;
+	(match name with None | Some "_" -> () | Some name -> ctx.idents <- PMap.add name ft ctx.idents);
+	mk (TMut expr) ft p
+
+let rec type_functions ctx =
+	let l = ctx.functions in
+	ctx.functions <- [];
+	let l = List.map (fun (expr,ft,el,e,rt,p) ->
+		let idents = ctx.idents in
+		List.iter (fun (p,pt) ->
+			if p <> "_" then ctx.idents <- PMap.add p pt ctx.idents;
+		) el;
+		let e = type_expr ctx e in
+		ctx.idents <- idents;
+		let ft2 = mk_fun ctx.gen (List.map snd el) e.etype in
+		unify ctx ft ft2 p;
+		expr := mk (TFunction (el,e)) ft2 p;
+		ft2
+	) (List.rev l) in
+	List.iter (polymorphize ctx.gen) l
+
+and type_expr ctx (e,p) =
 	match e with
 	| EConst c -> type_constant ctx c p
-	| EBlock [] -> error Invalid_expression p
+	| EBlock [] -> 
+		mk (TConst TVoid) t_void p
 	| EBlock (e :: l) ->
 		let idents = ctx.idents in
 		let e = List.fold_left (fun acc e ->
 			let e = type_block ctx e in
 			mk (TNext (acc,e)) e.etype (punion (pos acc) (pos e))
 		) (type_block ctx e) l in
+		type_functions ctx;
 		ctx.idents <- idents;
 		e
 	| ECall ((EConst (Constr "TYPE"),_),[e]) ->
 		let e = type_expr ctx e in
-		prerr_endline (s_type e.etype);
+		prerr_endline ("type : " ^ s_type ~ext:true e.etype);
 		e
 	| ECall (e,el) ->
 		let e = type_expr ctx e in
@@ -276,12 +335,9 @@ let rec type_expr ctx (e,p) =
 			with
 				Not_found -> error (Have_no_field (e.etype,s)) p)
 		| _ ->
-			try
-				let r , t , _ = record_field ctx s in
-				unify ctx e.etype r (pos e);
-				t
-			with
-				Not_found -> error (Custom ("Unknown field " ^ s)) p
+			let r , t , _ = record_field ctx s p in
+			unify ctx e.etype r (pos e);
+			t
 		) in
 		mk (TField (e,s)) t p
 	| EArray (e,ei) ->
@@ -306,30 +362,10 @@ let rec type_expr ctx (e,p) =
 		let e2 = type_expr ctx e2 in
 		unify ctx e2.etype e1.etype (pos e2);
 		mk (TIf (e,e1,Some e2)) e1.etype p
-	| EFunction (pl,e,rt) ->
-		let pl = (match pl with [] -> ["_",Some (EType ([],"void"))] | _ -> pl) in
-		let idents = ctx.idents in
-		let h = Hashtbl.create 0 in
-		let el = List.map (fun (pn,pt) ->
-			let t = t_mono() in
-			(match pt with
-			| None -> ()
-			| Some pt ->
-				let pt = type_type ~h ctx pt p in
-				unify ctx t pt p);
-			if pn <> "_" then ctx.idents <- PMap.add pn t ctx.idents;
-			pn , t
-		) pl in
-		let e = type_expr ctx e in
-		(match rt with 
-		| None -> () 
-		| Some rt -> 
-			let rt = type_type ctx rt p in
-			unify ctx e.etype rt p);
-		List.iter (fun (_,t) -> polymorphize ctx.gen t) el;
-		polymorphize ctx.gen e.etype;
-		ctx.idents <- idents;
-		mk (TFunction (el,e)) (mk_fun ctx.gen (List.map snd el) e.etype) p
+	| EFunction (name,pl,e,rt) ->
+		let r = register_function ctx name pl e rt p in
+		type_functions ctx;
+		r
 	| EBinop (op,e1,e2) ->
 		type_binop ctx op (type_expr ctx e1) (type_expr ctx e2) p
 	| ETypeAnnot (e,t) ->
@@ -346,17 +382,29 @@ let rec type_expr ctx (e,p) =
 		let el = List.map (type_expr ctx) el in
 		mk (TTupleDecl el) (mk_tup ctx.gen (List.map (fun e -> e.etype) el)) p
 	| ETypeDecl (params,tname,decl) ->
-		if Hashtbl.mem ctx.types tname then error (Custom ("Invalid type redefinition of " ^ tname)) p;
-		let h = Hashtbl.create 0 in
-		let tl = List.map (fun p ->
-			let t = t_mono() in
-			Hashtbl.add h p t;
-			t
-		) params in
-		let t = {
-			tid = -1;
-			texpr = TAbstract;
-		} in
+		let t , tl , h =
+			try
+				let t , tl , h = Hashtbl.find ctx.tmptypes tname in
+				if decl <> EAbstract then Hashtbl.remove ctx.tmptypes tname;
+				if List.length tl <> List.length params then error (Custom ("Invalid number of parameters for type " ^ tname)) p;
+				t , tl , h
+			with
+				Not_found ->
+					if Hashtbl.mem ctx.types tname then error (Custom ("Invalid type redefinition of type " ^ tname)) p;
+					let h = Hashtbl.create 0 in
+					let tl = List.map (fun p ->
+						let t = t_mono() in
+						Hashtbl.add h p t;
+						t
+					) params in
+					let t = {
+						tid = genid ctx.gen;
+						texpr = TNamed (tname,tl,{ tid = -1; texpr = TAbstract });
+					} in
+					Hashtbl.add ctx.types tname t;
+					if decl = EAbstract then Hashtbl.add ctx.tmptypes tname (t,tl,h);
+					t , tl , h
+		in
 		let t2 = (match decl with
 			| EAbstract -> { tid = -1; texpr = TAbstract }
 			| EAlias t -> type_type ~allow:false ~h ctx t p
@@ -382,12 +430,50 @@ let rec type_expr ctx (e,p) =
 		t.tid <- if t2.tid = -1 && params = [] then -1 else genid ctx.gen;
 		t.texpr <- TNamed (tname,tl,t2);
 		polymorphize ctx.gen t;
-		Hashtbl.add ctx.types tname t;
 		mk (TTypeDecl t) t_void p
+	| ERecordDecl fl ->
+		let s , _ = (try List.hd fl with _ -> assert false) in
+		let r , _ , _ = record_field ctx s p in
+		let fll = (match tlinks false r with
+			| TRecord fl -> fl
+			| _ -> assert false
+		) in
+		let fl2 = ref fll in
+		let rec loop f = function
+			| [] -> 
+				if List.exists (fun (f2,_,_) -> f = f2) fll then
+					error (Custom ("Duplicate declaration for field " ^ f)) p
+				else
+					error (Have_no_field (r,f)) p
+			| (f2,_,ft) :: l when f = f2 -> ft , l
+			| x :: l -> 
+				let t , l = loop f l in
+				t , x :: l
+		in
+		let el = List.map (fun (f,e) ->
+			let ft , fl2b = loop f !fl2 in
+			fl2 := fl2b;
+			let e = type_expr ctx e in
+			unify ctx e.etype ft (pos e);
+			(f , e)
+		) fl in
+		List.iter (fun (f,_,_) ->
+			error (Custom ("Missing field " ^ f ^ " in record declaration")) p;
+		) !fl2;
+		mk (TRecordDecl el) r p
+	| EListDecl el ->
+		let t , pt = t_poly ctx.gen "list" in
+		let el = List.map (fun e ->
+			let e = type_expr ctx e in
+			unify ctx e.etype pt (pos e);
+			e
+		) el in
+		mk (TListDecl el) t p
 
 and type_block ctx ((e,p) as x)  = 
 	match e with
 	| EVar (v,t,e) ->
+		type_functions ctx;
 		let e = type_expr ctx e in
 		(match t with
 		| None -> ()
@@ -396,7 +482,10 @@ and type_block ctx ((e,p) as x)  =
 			unify ctx t e.etype (pos e));
 		if v <> "_" then ctx.idents <- PMap.add v e.etype ctx.idents;
 		mk (TVar (v,e)) t_void p
+	| EFunction (name,pl,e,rt) ->
+		register_function ctx name pl e rt p
 	| _ ->
+		type_functions ctx;
 		type_expr ctx x
 
 let context() = 
@@ -406,6 +495,8 @@ let context() =
 		constr = Hashtbl.create 0;
 		records = Hashtbl.create 0;
 		types = Hashtbl.create 0;
+		tmptypes = Hashtbl.create 0;
+		functions = [];
 	} in
 	let poly name =
 		let t , _ = t_poly ctx.gen name in
