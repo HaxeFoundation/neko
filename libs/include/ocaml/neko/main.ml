96402b26f1190147b7136afb920cab0db6198c23
Nicolas Cannasse <ncannasse@gmail.com>
2005-05-31 21:19:42 +0000

    added nekoml

--- libs/include/ocaml/neko/main.ml
+++ libs/include/ocaml/neko/main.ml
@@ -14,15 +14,18 @@ let normalize_path p =
 		| '\\' | '/' -> p
 		| _ -> p ^ "/"
 
-let report (msg,p) etype printer =
+let genreport ep (msg,p) etype printer =
 	let error_printer file line =
 		match !print_style with
 		| StyleJava -> sprintf "%s:%d:" file line
 		| StyleMSVC -> sprintf "%s(%d):" file line
 	in
-	let epos = Lexer.get_error_pos error_printer p in
+	let epos = ep error_printer p in
 	prerr_endline (sprintf "%s : %s %s" epos etype (printer msg));
 	exit 1
+
+let report a b c = genreport Lexer.get_error_pos a b c
+let mlreport a b c = genreport Mllexer.get_error_pos a b c
 
 let switch_ext file ext =
 	try
@@ -74,6 +77,13 @@ let dump file =
 	Bytecode.dump ch data;
 	IO.close_out ch
 
+let nekoml file =
+	let ch = open_file file in
+	let ast = Mlparser.parse (Lexing.from_channel ch) file in
+	let ctx = Mltyper.context() in
+	let e = Mltyper.type_expr ctx ast in
+	()
+
 ;;
 try	
 	let usage = "Neko v0.3 - (c)2005 Nicolas Cannasse\n Usage : neko.exe [options] <files...>\n Options :" in
@@ -83,6 +93,7 @@ try
 		("-x", Arg.String interp,"<file> : interpret neko program");
 		("-c", Arg.String compile,"<file> : compile file to NekoVM bytecode");
 		("-d", Arg.String dump,"<file> : dump NekoVM bytecode");
+		("-nml", Arg.String nekoml,"<file> : compile NekoML file");
 	] in
 	Arg.parse args_spec (fun file -> raise (Arg.Bad file)) usage;
 with	
@@ -90,6 +101,9 @@ with
 	| Parser.Error (m,p) -> report (m,p) "parse error" Parser.error_msg
 	| Interp.Error (m,p) -> report (m,p) "runtime error" Interp.error_msg
 	| Compile.Error (m,p) -> report (m,p) "compile error" Compile.error_msg
+	| Mllexer.Error (m,p) -> mlreport (m,p) "syntax error" Mllexer.error_msg
+	| Mlparser.Error (m,p) -> mlreport (m,p) "parse error" Mlparser.error_msg
+	| Mltyper.Error (m,p) -> mlreport (m,p) "type error" Mltyper.error_msg
 	| Failure msg ->
 		prerr_endline msg;
 		exit 1;
