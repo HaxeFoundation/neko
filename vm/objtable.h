f38c45212a084151b4f22eb0957ec1b7f0410e36
Nicolas Cannasse <ncannasse@gmail.com>
2005-07-06 17:48:04 +0000

    both kind of tables.

--- vm/objtable.h
+++ vm/objtable.h
@@ -4,8 +4,34 @@
 /*  (c)2005 Nicolas Cannasse												*/
 /*																			*/
 /* ************************************************************************ */
-#pragma once
+#ifndef __OBJTABLE_H
+#define __OBJTABLE_H
 #include "neko.h"
+
+#ifndef COMPACT_TABLE
+
+struct _objtable {
+	field id;
+	value data;
+	struct _objtable *left;
+	struct _objtable *right;
+	int skew;
+};
+
+value *otable_find(objtable t,field id);
+
+#define otable_empty()	NULL
+#define otable_replace(t,id,data) _otable_replace(&(t),id,data)
+#define otable_remove(t,id) _otable_remove(&(t),id)
+#define otable_optimize(t) _otable_optimize(&(t))
+
+int otable_count(objtable t);
+unsigned int _otable_replace(objtable *t, field id, value data);
+unsigned int _otable_remove(objtable *t, field id);
+void _otable_optimize(objtable *t);
+
+
+#else
 
 typedef struct {
 	field id;
@@ -18,13 +44,15 @@ struct _objtable
 	cell *cells;
 };
 
-INLINE value *otable_find(objtable t,field id) {
-	int min = 0;
-	int max = t->count;
+static INLINE value *otable_find(objtable t,field id) {
+	int min;
+	int max;
 	int mid;
 	field cid;
-	if( !max )
+	if( !t->count )
 		return NULL;
+	max = t->count;
+	min = 0;
 	while( min < max ) {
 		mid = (min + max) >> 1;
 		cid = t->cells[mid].id;
@@ -40,8 +68,14 @@ INLINE value *otable_find(objtable t,field id) {
 
 objtable otable_empty();
 void otable_replace(objtable t, field id, value data);
-int otable_remove(objtable t, field id);
+void otable_remove(objtable t, field id);
 void otable_optimize(objtable t);
-objtable otable_copy(objtable t);
+#define otable_count(t)	(t)->count
 
+#endif
+
+objtable otable_copy(objtable t);
+void otable_iter(objtable t, void f( value data, field id, void *), void *p );
+
+#endif
 /* ************************************************************************ */
