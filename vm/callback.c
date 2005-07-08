7c08a0d54c1e87f33cf02522cd2df6db28b33d11
Nicolas Cannasse <ncannasse@gmail.com>
2005-07-08 12:52:13 +0000

    added val_callEx

--- vm/callback.c
+++ vm/callback.c
@@ -16,75 +16,94 @@ typedef value (*c_prim3)(value,value,value);
 typedef value (*c_prim4)(value,value,value,value);
 typedef value (*c_primN)(value*,int);
 
-EXTERN value val_callN( value f, value *args, int nargs ) {
+extern void neko_setup_trap( neko_vm *vm, int where );
+extern void neko_process_trap( neko_vm *vm );
+
+EXTERN value val_callEx( value this, value f, value *args, int nargs, value *exc ) {
+	neko_vm *vm = NEKO_VM();
+	value old_this = vm->val_this;
+	value ret = val_null;
+	jmp_buf oldjmp;
 	if( !val_is_function(f) || nargs < 0 )
 		return val_null;
+	if( this != NULL )
+		vm->val_this = this;
+	if( exc ) {
+		memcpy(&oldjmp,&vm->start,sizeof(jmp_buf));
+		if( setjmp(vm->start) ) {
+			*exc = vm->val_this;
+			neko_process_trap(vm);
+			vm->val_this = old_this;
+			memcpy(&vm->start,&oldjmp,sizeof(jmp_buf));
+			return val_null;
+		}
+		neko_setup_trap(vm,0);
+	}
 	if( val_tag(f) == VAL_PRIMITIVE ) {
-		neko_vm *vm = NEKO_VM();
-		value v;
 		value old_env = vm->env;
 		vm->env = ((vfunction *)f)->env;
 		if( nargs == ((vfunction*)f)->nargs ) {
 			switch( nargs ) {
 			case 0:
-				v = ((c_prim0)((vfunction*)f)->addr)();
+				ret = ((c_prim0)((vfunction*)f)->addr)();
 				break;
 			case 1:
-				v = ((c_prim1)((vfunction*)f)->addr)(args[0]);
+				ret = ((c_prim1)((vfunction*)f)->addr)(args[0]);
 				break;
 			case 2:
-				v = ((c_prim2)((vfunction*)f)->addr)(args[0],args[1]);
+				ret = ((c_prim2)((vfunction*)f)->addr)(args[0],args[1]);
 				break;
 			case 3:
-				v = ((c_prim3)((vfunction*)f)->addr)(args[0],args[1],args[2]);
+				ret = ((c_prim3)((vfunction*)f)->addr)(args[0],args[1],args[2]);
 				break;
 			case 4:
-				v = ((c_prim4)((vfunction*)f)->addr)(args[0],args[1],args[2],args[3]);
+				ret = ((c_prim4)((vfunction*)f)->addr)(args[0],args[1],args[2],args[3]);
 				break;
 			default:
-				v = val_null;
+				ret = val_null;
 				break;
 			}
 		} else if( ((vfunction*)f)->nargs == -1 )
-			v = (value)((c_primN)((vfunction*)f)->addr)(args,nargs);
-		if( v == NULL )
-			v = val_null;
-		vm->env = old_env;
-		return v;
+			ret = (value)((c_primN)((vfunction*)f)->addr)(args,nargs);
+		if( ret == NULL )
+			ret = val_null;
+		vm->env = old_env;		
 	} else if( val_tag(f) == VAL_FUNCTION ) {
 		if( nargs == ((vfunction*)f)->nargs )  {
-			neko_vm *vm = NEKO_VM();
 			int n;
-			value v;
 			if( vm->csp + 3 >= vm->sp - nargs ) {
-				vm->error = "OVERFLOW";
-				return val_null;
+				if( exc )
+					memcpy(&vm->start,&oldjmp,sizeof(jmp_buf));	
+				vm->val_this = old_this;
+				val_throw(alloc_string("OVERFLOW"));
+			} else {
+				for(n=0;n<nargs;n++)
+					*--vm->sp = (unsigned int)args[n];
+				*++vm->csp = (int)callback_return;
+				*++vm->csp = 0;
+				*++vm->csp = (int)vm->val_this;
+				ret = interp(vm,(int)val_null,(int*)((vfunction*)f)->addr,((vfunction*)f)->env);
 			}
-			for(n=0;n<nargs;n++)
-				*--vm->sp = (unsigned int)args[n];
-			*++vm->csp = (int)callback_return;
-			*++vm->csp = 0;
-			*++vm->csp = (int)vm->val_this;
-			v = interp(vm,(int)val_null,(int*)((vfunction*)f)->addr,((vfunction*)f)->env);
-			if( vm->error != NULL )
-				return val_null;
-			return v;
 		}
 	}
-	return val_null;
+	if( exc )
+		memcpy(&vm->start,&oldjmp,sizeof(jmp_buf));	
+	vm->val_this = old_this;
+	return ret;
+}
+
+EXTERN value val_callN( value f, value *args, int nargs ) {
+	return val_callEx(NULL,f,args,nargs,NULL);
 }
 
 EXTERN value val_ocallN( value o, field f, value *args, int nargs ) {
-	neko_vm *vm = NEKO_VM();
-	value *meth = otable_find(((vobject*)o)->table,f);
-	value old_this = vm->val_this;
-	value r;
+	value *meth;
+	if( !val_is_object(o) )
+		return val_null;
+	meth = otable_find(((vobject*)o)->table,f);
 	if( meth == NULL )
 		return val_null;
-	vm->val_this = o;
-	r = val_callN(*meth,args,nargs);
-	vm->val_this = old_this;
-	return r;
+	return val_callEx(o,*meth,args,nargs,NULL);
 }
 
 EXTERN value val_call0( value f ) {
