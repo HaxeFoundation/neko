804f63e793873cc1c0dd5eb8eb8f477d4c223803
Nicolas Cannasse <ncannasse@gmail.com>
2005-07-19 16:35:06 +0000

    done.

--- libs/mysql/mysql.c
+++ libs/mysql/mysql.c
@@ -17,7 +17,7 @@ static void error( MYSQL *m, const char *msg ) {
 
 // ---------------------------------------------------------------
 // Result
-/*
+
 #undef CONV_FLOAT
 typedef enum {
 	CONV_INT,
@@ -38,50 +38,31 @@ static field current_id;
 static void free_result( value o ) {
 	result *r = RESULT(o);
 	mysql_free_result(r->r);
-	free(r->fields_ids);
-	free(r->fields_convs);
-	free(r);
 }
 
-static value result_toString() {
-	value o = val_this();
-	val_check_obj(o,t_result);
-	return alloc_string("#Sql.Result");
-}
-
-static value empty_result_toString() {
-	return alloc_string("#Sql.EmptyResult");
-}
-
-static value empty_result_get_length() {
-	return val_field(val_this(),val_id("_n"));
-}
-
-static value result_get_length() {
-	value o = val_this();
-	val_check_obj(o,t_result);
+static value result_get_length( value o ) {
+	if( val_is_int(o) )
+		return o;
+	val_check_kind(o,k_result);
 	return alloc_int( (int)mysql_num_rows(RESULT(o)->r) );
 }
 
-static value result_get_nfields() {
-	value o = val_this();
-	val_check_obj(o,t_result);
+static value result_get_nfields( value o ) {
+	val_check_kind(o,k_result);
 	return alloc_int(RESULT(o)->nfields);
 }
 
-static value result_next() {
-	value o = val_this();
+static value result_next( value o ) {
 	result *r;
 	MYSQL_ROW row;
-	val_check_obj(o,t_result);
+	val_check_kind(o,k_result);
 	r = RESULT(o);
 	row = mysql_fetch_row(r->r);
 	if( row == NULL )
-		return val_false;
+		return val_null;
 	{
 		int i;
 		value cur = alloc_object(NULL);
-		alloc_field(o,current_id,cur);
 		r->current = row;
 		for(i=0;i<r->nfields;i++)
 			if( row[i] != NULL ) {
@@ -102,20 +83,19 @@ static value result_next() {
 				}
 				alloc_field(cur,r->fields_ids[i],v);
 			}
-		return val_true;
+		return cur;
 	}
 }
 
-static value result_getResult( value n ) {
-	value o = val_this();
+static value result_get( value o, value n ) {
 	result *r;
 	const char *s;
-	val_check_obj(o,t_result);
+	val_check_kind(o,k_result);
 	r = RESULT(o);
 	if( !val_is_int(n) || val_int(n) < 0  || val_int(n) >= r->nfields )
 		return NULL;
 	if( !r->current ) {
-		result_next();
+		result_next(o);
 		if( !r->current )
 			return val_null;
 	}
@@ -123,17 +103,16 @@ static value result_getResult( value n ) {
 	return alloc_string( s?s:"" );
 }
 
-static value result_getIntResult( value n ) {
-	value o = val_this();
+static value result_get_int( value o, value n ) {
 	value cur;
 	result *r;
 	const char *s;
-	val_check_obj(o,t_result);
+	val_check_kind(o,k_result);
 	r = RESULT(o);
 	if( !val_is_int(n) || val_int(n) < 0  || val_int(n) >= r->nfields )
 		return NULL;
 	if( !r->current ) {
-		result_next();
+		result_next(o);
 		if( !r->current )
 			return val_null;
 	}
@@ -142,34 +121,20 @@ static value result_getIntResult( value n ) {
 	return alloc_int( s?atoi(s):0 );
 }
 
-static value result_getFloatResult( value n ) {
-	value o = val_this();
+static value result_get_float( value o, value n ) {
 	result *r;
 	const char *s;
-	val_check_obj(o,t_result);
+	val_check_kind(o,k_result);
 	r = RESULT(o);
 	if( !val_is_int(n) || val_int(n) < 0  || val_int(n) >= r->nfields )
 		return NULL;
 	if( !r->current ) {
-		result_next();
+		result_next(o);
 		if( !r->current )
 			return val_null;
 	}
 	s = r->current[val_int(n)];
 	return alloc_float( s?atof(s):0 );
-}
-
-static value std_mysql_result() {
-	value o = alloc_class(&t_result);
-	Method(o,result,get_nfields,0);
-	Method(o,result,toString,0);
-	Method(o,result,get_length,0);
-	Method(o,result,getResult,1);
-	Method(o,result,getIntResult,1);
-	Method(o,result,getFloatResult,1);
-	Method(o,result,next,0);
-	current_id = val_id("current");	
-	return o;
 }
 
 static CONV convert_type( enum enum_field_types t ) {
@@ -188,34 +153,34 @@ static CONV convert_type( enum enum_field_types t ) {
 }
 
 static value alloc_result( MYSQL_RES *r ) {
-	value o = alloc_object(&t_result);
+	result *res = (result*)alloc(sizeof(result));
+	value o = alloc_abstract(k_result,res);
 	int num_fields = mysql_num_fields(r);
 	int i,j;
 	MYSQL_FIELD *fields = mysql_fetch_fields(r);
-	result *res = (result*)alloc(sizeof(result));
 	res->r = r;
 	res->current = NULL;
 	res->nfields = num_fields;
-	res->fields_ids = (field*)alloc_abstract(sizeof(field)*num_fields);
-	res->fields_convs = (CONV*)alloc_abstract(sizeof(CONV)*num_fields);
+	res->fields_ids = (field*)alloc_private(sizeof(field)*num_fields);
+	res->fields_convs = (CONV*)alloc_private(sizeof(CONV)*num_fields);
 	for(i=0;i<num_fields;i++) {
 		field id = val_id(fields[i].name);
 		for(j=0;j<i;j++)
 			if( res->fields_ids[j] == id ) {
-				val_print(alloc_string("Error, same field ids for : <b>"));
-				val_print(alloc_string(fields[i].name));
-				val_print(alloc_string(":"));
-				val_print(alloc_int(i));
-				val_print(alloc_string("</b> and <b>"));
-				val_print(alloc_string(fields[j].name));
-				val_print(alloc_string(":"));
-				val_print(alloc_int(j));
-				val_print(alloc_string("</b>.<br/>"));
+				buffer b = alloc_buffer("Error, same field ids for : <b>");
+				buffer_append(b,fields[i].name);
+				buffer_append(b,":");
+				val_buffer(b,alloc_int(i));
+				buffer_append(b,"</b> and <b>");
+				buffer_append(b,fields[j].name);
+				buffer_append(b,":");
+				val_buffer(b,alloc_int(j));
+				buffer_append(b,"</b>.<br/>");
+				val_throw(buffer_to_string(b));
 			}
 		res->fields_ids[i] = id;
 		res->fields_convs[i] = convert_type(fields[i].type); 
 	}
-	val_odata(o) = (value)res;
 	val_gc(o,free_result);
 	return o;
 }
@@ -223,21 +188,19 @@ static value alloc_result( MYSQL_RES *r ) {
 // ---------------------------------------------------------------
 // Connection
 
-static value connection_close() {
-	value o = val_this();
-	val_check_obj(o,t_connection);
+static value close( value o ) {
+	val_check_kind(o,k_connection);
 	mysql_close(MYSQLDATA(o));
-	val_odata(o) = NULL;
-	val_otype(o) = NULL;
+	val_data(o) = NULL;
+	val_kind(o) = NULL;
 	val_gc(o,NULL);
 	return val_true;
 }
 
-static value connection_selectDB( value db ) {
-	value o = val_this();
-	val_check_obj(o,t_connection);
+static value selectDB( value o, value db ) {
+	val_check_kind(o,k_connection);
 	if( !val_is_string(db) )
-		return NULL;
+		return val_null;
 	if( mysql_select_db(MYSQLDATA(o),val_string(db)) != 0 ) {
 		error(MYSQLDATA(o),"Failed to select database :");
 		return val_false;
@@ -245,43 +208,27 @@ static value connection_selectDB( value db ) {
 	return val_true;
 }
 
-static value connection_request( value r )  {
-	value o = val_this();
+static value request( value o, value r )  {
 	MYSQL_RES *res;
-	val_check_obj(o,t_connection);
+	val_check_kind(o,k_connection);
 	if( !val_is_string(r) )
-		return NULL;
+		return val_null;
 	if( mysql_real_query(MYSQLDATA(o),val_string(r),val_strlen(r)) != 0 ) {
 		error(MYSQLDATA(o),val_string(r));
 		return val_null;
 	}
 	res = mysql_store_result(MYSQLDATA(o));
 	if( res == NULL ) {
-		if( mysql_field_count(MYSQLDATA(o)) == 0 ) {
-			// no result : insert for example
-			int n = (int)mysql_affected_rows(MYSQLDATA(o));
-			o = alloc_object(NULL);
-			alloc_field(o,val_id("_n"),alloc_int(n));
-			Method(o,empty_result,toString,0);
-			Method(o,empty_result,get_length,0);
-			return o;
-		} else {
+		if( mysql_field_count(MYSQLDATA(o)) == 0 )
+			return alloc_int( (int)mysql_affected_rows(MYSQLDATA(o)) );
+		else {
 			error(MYSQLDATA(o),val_string(r));
 			return val_null;
 		}
-	}
+	}	
 	return alloc_result(res);
 }
 
-value std_mysql_connection() {
-	value o = alloc_class(&t_connection);
-	Method(o,connection,selectDB,1);
-	Method(o,connection,request,1);
-	Method(o,connection,close,0);
-	return o;
-}
-
-*/
 // ---------------------------------------------------------------
 // Sql
 
@@ -293,7 +240,7 @@ static void free_connection( value o ) {
 value connect( value params  ) {
 	value host, port, user, pass, socket;
 	if( !val_is_object(params) )
-		return NULL;
+		return val_null;
 	host = val_field(params,val_id("host"));
 	port = val_field(params,val_id("port"));
 	user = val_field(params,val_id("user"));
@@ -306,7 +253,7 @@ value connect( value params  ) {
 		val_is_string(pass) &&
 		(val_is_string(socket) || val_is_null(socket))
 	) )
-		return NULL;
+		return val_null;
 	{
 		MYSQL *m = mysql_init(NULL);
 		value v;
@@ -321,4 +268,23 @@ value connect( value params  ) {
 	}	
 }
 
+// ---------------------------------------------------------------
+// Registers
+
+static void init() {
+	current_id = val_id("current");
+}
+
+DEFINE_ENTRY_POINT(init);
+
 DEFINE_PRIM(connect,1);
+DEFINE_PRIM(close,1);
+DEFINE_PRIM(request,2);
+DEFINE_PRIM(selectDB,2);
+
+DEFINE_PRIM(result_get_length,1);
+DEFINE_PRIM(result_get_nfields,1);
+DEFINE_PRIM(result_next,1);
+DEFINE_PRIM(result_get,2);
+DEFINE_PRIM(result_get_int,2);
+DEFINE_PRIM(result_get_float,2);
