0cd175ebb8dcf6fa6d1157f3309e5a505786a3fb
Nicolas Cannasse <ncannasse@gmail.com>
2005-08-09 16:15:41 +0000

    fixed mult bug.

--- vm/interp.c
+++ vm/interp.c
@@ -218,7 +218,7 @@ typedef int (*c_primN)(value*,int);
 		Next
 
 #define SUB(x,y) ((x) - (y))
-#define MULT(x,y) ((x) - (y))
+#define MULT(x,y) ((x) * (y))
 #define DIV(x,y) ((x) / (y))
 
 #define NumberOp(op,fop,id_op,id_rop) \
