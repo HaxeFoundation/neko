07d12a89742565dc4bcf0f3d7ca568a37aa7eb25
Nicolas Cannasse <ncannasse@gmail.com>
2005-08-10 16:07:59 +0000

    plugin architecture

--- libs/include/ocaml/neko/main.ml
+++ libs/include/ocaml/neko/main.ml
@@ -14,93 +14,56 @@ let normalize_path p =
 		| '\\' | '/' -> p
 		| _ -> p ^ "/"
 
-let genreport ep (msg,p) etype printer =
-	let error_printer file line =
-		match !print_style with
+let report inf =
+	let pos file line = (match !print_style with
 		| StyleJava -> sprintf "%s:%d:" file line
 		| StyleMSVC -> sprintf "%s(%d):" file line
-	in
-	let epos = ep error_printer p in
-	prerr_endline (sprintf "%s : %s %s" epos etype (printer msg));
+	) in
+	prerr_endline (sprintf "%s : %s %s" (inf.Plugin.exn_pos pos) inf.Plugin.exn_name inf.Plugin.exn_message);
 	exit 1
 
-let report a b c = genreport Lexer.get_error_pos a b c
-(*//let report a b c = genreport Mllexer.get_error_pos a b c*)
-
-let switch_ext file ext =
-	try
-		Filename.chop_extension file ^ ext
-	with
-		_ -> file ^ ext
-
-let open_file ?(bin=false) file =
-	try 
-		if bin then open_in_bin file else open_in file
-	with _ -> failwith ("File not found " ^ file)
-
-(*/*
-let interp file =
-	let ctx = Interp.create ["";normalize_path (Filename.dirname file)] in
-	let mname = switch_ext (Filename.basename file) "" in
-	try
-		ignore(Interp.execute ctx mname Ast.null_pos);
-	with
-		Interp.Error (Interp.Module_not_found m,_) when m = mname -> 
-			failwith ("File not found " ^ file)
-*/*)
-
-let compile file =
-	let ch = open_file file in
-	let ast = Parser.parse (Lexing.from_channel ch) file in
-	close_in ch;
-	let data = Compile.compile file ast in
-	let file = switch_ext file ".n" in
-	let ch = IO.output_channel (open_out_bin file) in
-	Bytecode.write ch data;
-	IO.close_out ch
-
-let dump file =
-	let ch = IO.input_channel (open_file ~bin:true file) in
+let dump file ch out =
 	let data = (try Bytecode.read ch with Bytecode.Invalid_file -> IO.close_in ch; failwith ("Invalid bytecode file " ^ file)) in
 	IO.close_in ch;
-	let fout = switch_ext file ".txt" in
-	let ch = IO.output_channel (open_out fout) in
-	Bytecode.dump ch data;
-	IO.close_out ch
+	Bytecode.dump out data;
+	IO.close_out out
 
-(*/*
-let nekoml file =
-	let ctx = Mltyper.context ["";Filename.dirname file ^ "/"] in
-	ignore(Mltyper.load_module ctx [String.capitalize (Filename.chop_extension (Filename.basename file))] Mlast.null_pos);
-	Hashtbl.iter (fun m e ->
-		let e = Mlneko.generate e in
-		let file = String.concat "/" m ^ ".neko" in
-		let ch = IO.output_channel (open_out file) in
-		let ctx = Printer.create ch in
-		Printer.print ctx e;
-		IO.close_out ch
-	) (Mltyper.modules ctx)
-*/*)
+let dump_exn = function
+	| e -> raise e
+
+let compile file ch out =
+	let ast = Parser.parse (Lexing.from_function (fun s p -> try IO.input ch s 0 p with IO.No_more_input -> 0)) file in
+	IO.close_in ch;
+	let data = Compile.compile file ast in
+	Bytecode.write out data;
+	IO.close_out out
+
+let compile_exn = function
+	| Lexer.Error (m,p) -> Plugin.exn_infos "syntax error" (Lexer.error_msg m) (fun f -> Lexer.get_error_pos f p)
+	| Parser.Error (m,p) -> Plugin.exn_infos "parse error" (Parser.error_msg m) (fun f -> Lexer.get_error_pos f p)
+	| Compile.Error (m,p) -> Plugin.exn_infos "compile error" (Compile.error_msg m) (fun f -> Lexer.get_error_pos f p)
+	| e -> raise e
+
+let main() =
+	try	
+		let usage = "Neko v0.4 - (c)2005 Nicolas Cannasse\n Usage : neko.exe [options] <files...>\n Options :" in
+		let output = ref "n" in
+		let args_spec = [
+			("-msvc",Arg.Unit (fun () -> print_style := StyleMSVC),": use MSVC style errors");
+			("-p", Arg.String (fun p -> Plugin.add_path p),"<path> : add the file to path");
+			("-o", Arg.String (fun ext -> output := String.lowercase ext),"<file> : specify output extension");
+		] in
+		Arg.parse args_spec (fun file -> 
+			Plugin.generate file !output
+		) usage;
+	with
+		| Plugin.Error inf ->
+			report inf
+		| Failure msg ->
+			prerr_endline msg;
+			exit 1
 
 ;;
-try	
-	let usage = "Neko v0.3 - (c)2005 Nicolas Cannasse\n Usage : neko.exe [options] <files...>\n Options :" in
-	let args_spec = [
-		("-msvc",Arg.Unit (fun () -> print_style := StyleMSVC),": use MSVC style errors");
-(*//	("-x", Arg.String interp,"<file> : interpret neko program"); *)
-		("-c", Arg.String compile,"<file> : compile file to NekoVM bytecode");
-		("-d", Arg.String dump,"<file> : dump NekoVM bytecode");
-(*//	("-nml", Arg.String nekoml,"<file> : compile NekoML file"); *)
-	] in
-	Arg.parse args_spec (fun file -> raise (Arg.Bad file)) usage;
-with	
-	| Lexer.Error (m,p) -> report (m,p) "syntax error" Lexer.error_msg
-	| Parser.Error (m,p) -> report (m,p) "parse error" Parser.error_msg
-	| Compile.Error (m,p) -> report (m,p) "compile error" Compile.error_msg
-(*/*| Interp.Error (m,p) -> report (m,p) "runtime error" Interp.error_msg
-	| Mllexer.Error (m,p) -> mlreport (m,p) "syntax error" Mllexer.error_msg
-	| Mlparser.Error (m,p) -> mlreport (m,p) "parse error" Mlparser.error_msg
-	| Mltyper.Error (m,p) -> mlreport (m,p) "type error" Mltyper.error_msg */*)
-	| Failure msg ->
-		prerr_endline msg;
-		exit 1;
+Plugin.register "neko" "n" compile compile_exn;
+Plugin.register "n" "dump" dump dump_exn;
+at_exit main
