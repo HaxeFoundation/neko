9f65d4c3fccafa826395942708b8eeb54d225a6d
Nicolas Cannasse <ncannasse@gmail.com>
2005-08-22 13:17:03 +0000

    added new make system.

--- mkrelease.bat
+++ mkrelease.bat
@@ -1,8 +1,9 @@
 @echo off
 
 set VERSION=neko-1.0.5
+set FILES=libs neko bin vm mk LICENSE INSTALL Makefile configure
 
-tar --exclude=neko/lexer.ml --exclude=.cvsignore --exclude=neko/*.cm* --exclude=neko/*.o* --exclude=*.tgz --exclude=CVS --exclude=libs/mtypes --exclude=bin/* --exclude=*.sln --exclude=*.vcproj --exclude=*.suo --exclude=*.ncb --exclude=debug --exclude=release --exclude=libs/include -zcf %VERSION%.tgz libs neko bin vm LICENSE Makefile
+tar --exclude=neko/lexer.ml --exclude=.cvsignore --exclude=neko/*.cm* --exclude=neko/*.o* --exclude=*.tgz --exclude=CVS --exclude=libs/mtypes --exclude=bin/* --exclude=*.sln --exclude=*.vcproj --exclude=*.suo --exclude=*.ncb --exclude=debug --exclude=release --exclude=libs/include -zcf %VERSION%.tgz %FILES%
 mkdir %VERSION%
 tar -C %VERSION% -zxf %VERSION%.tgz
 tar -zcf %VERSION%.tgz %VERSION%
