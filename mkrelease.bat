5a448034389bd1f87c4ad3f4d417264597c0c853
Nicolas Cannasse <ncannasse@gmail.com>
2005-08-25 10:08:34 +0000

    added CHANGES.

--- mkrelease.bat
+++ mkrelease.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set VERSION=neko-1.0.5
-set FILES=libs neko bin vm mk LICENSE INSTALL Makefile configure
+set FILES=libs neko bin vm mk LICENSE INSTALL CHANGES Makefile configure
 
 tar --exclude=neko/lexer.ml --exclude=.cvsignore --exclude=neko/*.cm* --exclude=neko/*.o* --exclude=*.tgz --exclude=CVS --exclude=libs/mtypes --exclude=bin/* --exclude=*.sln --exclude=*.vcproj --exclude=*.suo --exclude=*.ncb --exclude=debug --exclude=release --exclude=libs/include -zcf %VERSION%.tgz %FILES%
 mkdir %VERSION%
