8cf620acb28cc7ce7ab5836e8041f0d06cb7332f
Nicolas Cannasse <ncannasse@gmail.com>
2005-09-20 14:37:59 +0000

    minor additions to pattern matching representation.

--- libs/include/ocaml/ml/mltype.ml
+++ libs/include/ocaml/ml/mltype.ml
@@ -52,11 +52,13 @@ type tconstant =
 and match_op =
 	| MFailure
 	| MHandle of match_op * match_op
+	| MEval of texpr
 	| MExecute of texpr
 	| MConstants of match_op * (tconstant * match_op) list
 	| MField of match_op * int
 	| MSwitch of match_op * (tconstant * match_op) list
 	| MBind of string * match_op * match_op
+	| MWhen of texpr * match_op
 
 and texpr_decl =
 	| TConst of tconstant
