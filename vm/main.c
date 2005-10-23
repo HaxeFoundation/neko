a5ae96358063a347fbf67b2b0b6697fe4d1210f3
Nicolas Cannasse <ncannasse@gmail.com>
2005-10-23 06:54:19 +0000

    fixed warning.

--- vm/main.c
+++ vm/main.c
@@ -49,7 +49,7 @@ int main( int argc, char *argv[] ) {
 	int r;
 	_CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_DELAY_FREE_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 	neko_global_init();
-	p.args = argv + 1;
+	p.args = (const char **)argv + 1;
 	p.nargs = argc - 1;
 	p.custom = NULL;
 	p.printer = NULL;
