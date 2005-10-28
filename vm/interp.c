e697f27bae23db8e6c9f866c6371576c7abd8bb9
Nicolas Cannasse <ncannasse@gmail.com>
2005-10-28 13:35:55 +0000

    minor.

--- vm/interp.c
+++ vm/interp.c
@@ -551,7 +551,7 @@ static int_val interp_loop( neko_vm *vm, neko_module *m, int_val _acc, int_val *
 				val_throw(alloc_string("$apply"));
 			{
 				int i = fargs;
-				ACC_BACKUP;
+				ACC_BACKUP
 				value env = alloc_array(fargs + 1);
 				ACC_RESTORE;
 				val_array_ptr(env)[0] = (value)acc;
