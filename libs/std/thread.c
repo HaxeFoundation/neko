fac7149b139d7263f3aaba3644f779d42035049a
Nicolas Cannasse <ncannasse@gmail.com>
2006-10-25 15:08:55 +0000

    pthread implementation.

--- libs/std/thread.c
+++ libs/std/thread.c
@@ -18,10 +18,28 @@
 #include <neko_vm.h>
 #ifdef NEKO_WINDOWS
 #	include <windows.h>
-#	define val_thread(t)	((DWORD)(int_val)val_data(t))
-#	define val_lock(l)		((HANDLE)val_data(l))
 #	define WM_TMSG			(WM_USER + 1)
+
+typedef HANDLE vlock;
+
+#else
+#	include <pthread.h>
+
+typedef struct _tqueue {
+	value msg;
+	struct _tqueue *next;
+} tqueue;
+
+typedef struct _vlock {
+	pthread_mutex_t lock;
+	pthread_mutex_t wait;
+	int counter;
+} *vlock;
+
 #endif
+
+#define val_thread(t)	((vthread*)val_data(t))
+#define val_lock(l)		((vlock)val_data(l))
 
 DEFINE_KIND(k_thread);
 DEFINE_KIND(k_lock);
@@ -30,39 +48,86 @@ typedef struct {
 #	ifdef NEKO_WINDOWS
 	HANDLE thandle;
 	HANDLE tlock;
-#	else
+	DWORD tid;
+#	else	
+	pthread_t phandle;
+	tqueue *first;
+	tqueue *last;	
+	pthread_mutex_t qlock;
+	pthread_mutex_t qwait;
 #	endif
 	value callb;
 	value callparam;
 } vthread;
 
+#ifndef NEKO_WINDOWS
+
+static pthread_key_t local_thread = NULL;
+
+static void free_key( void *r ) {	
+	free_root((value*)r);
+}
+
+static vthread *get_local_thread() {
+	value *r;
+	if( local_thread == NULL )
+		pthread_key_create(&local_thread,free_key);
+	r = (value*)pthread_getspecific(local_thread);
+	return r?*r:NULL;
+}
+
+static void set_local_thread( vthread *t ) {
+	value *r = (value*)pthread_getspecific(local_thread);
+	if( r == NULL ) {
+		r = alloc_root(1);
+		pthread_setspecific(local_thread,r);
+	}
+	*r = t;
+}
+
+static void init_thread_queue( vthread *t ) {
+	pthread_mutex_init(&t->qlock,NULL);
+	pthread_mutex_init(&t->qwait,NULL);
+	pthread_mutex_lock(&t->qwait,NULL);
+}
+
+#endif
+
+
 #ifdef NEKO_WINDOWS
 static DWORD WINAPI ThreadMain( void *_t ) {
+#else
+static void *ThreadMain( void *_t ) {
+#endif
 	vthread *t = (vthread*)_t;
 	value exc = NULL;
 	neko_vm *vm;
+#	ifdef NEKO_WINDOWS
 	// this will create the thread message queue
 	PeekMessage(NULL,NULL,0,0,0);
 	// now we can give back control to the main thread
 	ReleaseSemaphore(t->tlock,1,NULL);
+#	else
+	set_local_thread(t);
+#	endif
 	// init and run the VM
 	vm = neko_vm_alloc(NULL);
 	neko_vm_select(vm);
 	val_callEx(val_null,t->callb,&t->callparam,1,&exc);
 	// cleanup
 	vm = NULL;
+#	ifdef NEKO_WINDOWS
 	CloseHandle(t->thandle);
+#	endif
 	return 0;
 }
-#endif
 
 /**
 	thread_create : f:function:1 -> p:any -> 'thread
 	<doc>Creates a thread that will be running the function [f(p)]</doc>
 **/
 static value thread_create( value f, value param ) {
-	vthread *t;
-	void *tid;
+	vthread *t;	
 	val_check_function(f,1);
 	t = (vthread*)alloc(sizeof(vthread));
 	memset(t,0,sizeof(vthread));
@@ -72,11 +137,7 @@ static value thread_create( value f, value param ) {
 	t->tlock = CreateSemaphore(NULL,0,1,NULL);
 	if( t->tlock == NULL )
 		neko_error();
-	{
-		DWORD id;
-		t->thandle = CreateThread(NULL,0,ThreadMain,t,0,&id);
-		tid = (void*)(int_val)id;
-	}
+	t->thandle = CreateThread(NULL,0,ThreadMain,t,0,&t->tid);
 	if( t->thandle == NULL ) {
 		CloseHandle(t->tlock);
 		neko_error();
@@ -84,9 +145,12 @@ static value thread_create( value f, value param ) {
 	WaitForSingleObject(t->tlock,INFINITE);
 	CloseHandle(t->tlock);
 #	else
-	neko_error();
+	get_local_thread(); // ensure that the key is initialized
+	init_thread_queue(t);
+	if( pthread_create(&t->phandle,NULL,&ThreadMain,t) != 0 )
+		neko_error();
 #	endif
-	return alloc_abstract(k_thread,tid);
+	return alloc_abstract(k_thread,t);
 }
 
 /**
@@ -94,29 +158,51 @@ static value thread_create( value f, value param ) {
 	<doc>Returns the current thread</doc>
 **/
 static value thread_current() {
-	void *tid;
 #	ifdef NEKO_WINDOWS
-	tid = (void*)(int_val)GetCurrentThreadId();
+	vthread *t = (vthread*)alloc(sizeof(vthread));
+	memset(t,0,sizeof(vthread));
+	t->tid = GetCurrentThreadId();
 #	else
-	neko_error();
+	vthread *t = get_local_thread();
+	if( t == NULL ) {
+		t = (vthread*)alloc(sizeof(vthread));
+		init_thread_queue(t);
+		set_local_thread(t);
+		t->phandle = pthread_self();
+	}
 #	endif
-	return alloc_abstract(k_thread,tid);
+	return alloc_abstract(k_thread,t);
 }
 
 /**
 	thread_send : 'thread -> msg:any -> void
 	<doc>Send a message into the target thread message queue</doc>
 **/
-static value thread_send( value t, value msg ) {
-	value *r;
-	val_check_kind(t,k_thread);
-	r = alloc_root(1);
-	*r = msg;
+static value thread_send( value vt, value msg ) {
+	vthread *t;
+	val_check_kind(vt,k_thread);
+	t = val_thread(vt);
 #	ifdef NEKO_WINDOWS
-	if( !PostThreadMessage(val_thread(t),WM_TMSG,0,(LPARAM)r) )
-		neko_error();
+	{
+		value *r = alloc_root(1);
+		*r = msg;
+		if( !PostThreadMessage(t->tid,WM_TMSG,0,(LPARAM)r) )
+			neko_error();
+	}
 #	else
-	neko_error();
+	{
+		tqueue *q = (tqueue*)alloc(sizeof(tqueue));
+		q->msg = msg;
+		q->next = NULL;
+		pthread_mutex_lock(t->qlock);
+		if( t->last == NULL )
+			t->first = q;
+		else
+			t->last->next = q
+		t->last = q;
+		pthread_mutex_unlock(t->qwait);
+		pthread_mutex_unlock(t->qlock);
+	}
 #	endif
 	return val_null;
 }
@@ -151,8 +237,27 @@ static value thread_read_message( value block ) {
 		break;
 	}
 #	else
+	vthread *t = val_thread(thread_current());
 	val_check(block,bool);
-	neko_error();
+	while( true ) {
+		value msg = NULL;
+		if( val_bool(block) )
+			pthread_mutex_lock(t->qwait);
+		pthread_mutex_lock(t->qlock);
+		if( t->first != NULL ) {
+			msg = t->first->msg;
+			t->first = t->first->next;
+			if( t->first == NULL )
+				t->last = NULL;
+			else
+				pthread_mutex_unlock(t->qwait);
+		}
+		pthread_mutex_unlock(t->qlock);
+		if( msg != NULL )
+			return msg;
+		if( !val_bool(block) )
+			break;
+	}
 #	endif
 	return val_null;
 }
@@ -161,22 +266,27 @@ static void free_lock( value l ) {
 #	ifdef NEKO_WINDOWS
 	CloseHandle( val_lock(l) );
 #	else
+	pthread_mutex_destroy( val_lock(l)->wait );
+	pthread_mutex_destroy( val_lock(l)->lock );
 #	endif
 }
 
 /**
 	lock_create : void -> 'lock
-	<doc>Creates a lock, initialy owned by the current thread</doc>
+	<doc>Creates a lock which is initially locked</doc>
 **/
 static value lock_create() {
 	value l;
-	void *data;
+	vlock data;
 #	ifdef NEKO_WINDOWS
 	data = CreateSemaphore(NULL,0,(1 << 10),NULL);
 	if( data == NULL )
 		neko_error();
 #	else
-	neko_error();
+	data = (vlock)alloc_private(sizeof(struct _vlock));
+	data->counter = 0;
+	if( pthread_mutex_init(&data->lock,NULL) != 0 || pthread_mutex_init(&data->wait,NULL) != 0 )
+		neko_error();
 #	endif
 	l = alloc_abstract(k_lock,data);
 	val_gc(l,free_lock);
@@ -185,17 +295,24 @@ static value lock_create() {
 
 /**
 	lock_release : 'lock -> void
-	<doc>Release a lock. The thread does not need to own the lock to be
+	<doc>
+	Release a lock. The thread does not need to own the lock to be
 	able to release it. If a lock is released several times, it can be
-	acquired as many times.</doc>
+	acquired as many times
+	</doc>
 **/
 static value lock_release( value lock ) {
+	vlock l; 
 	val_check_kind(lock,k_lock);
+	l = val_lock(lock);
 #	ifdef NEKO_WINDOWS
-	if( !ReleaseSemaphore(val_lock(lock),1,NULL) )
+	if( !ReleaseSemaphore(l,1,NULL) )
 		neko_error();
 #	else
-	neko_error();
+	pthread_mutex_lock(l->lock);
+	l->counter++;
+	pthread_mutex_unlock(l->wait);
+	pthread_mutex_unlock(l->lock);
 #	endif
 	return val_true;
 }
@@ -205,7 +322,7 @@ static value lock_release( value lock ) {
 	<doc>
 	Waits for a lock to be released and acquire it.
 	If [timeout] (in seconds) is not null and expires then
-	the returned value is false.
+	the returned value is false
 	</doc>	
 **/
 static value lock_wait( value lock, value timeout ) {
@@ -223,8 +340,26 @@ static value lock_wait( value lock, value timeout ) {
 	default:
 		neko_error();
 	}
-#	endif
-	return val_null;
+#	else
+	{
+		vlock l = val_lock(lock);
+		int found = 0;
+		// has_timeout : set alarm
+		while( !found ) {			
+			pthread_mutex_lock(l->wait);
+			pthread_mutex_lock(l->lock);
+			if( t->counter > 0 ) {
+				t->counter--;
+				if( t->counter > 0 )
+					pthread_mutex_unlock(l->wait);
+				found = 1;
+			}
+			pthread_mutex_unlock(t->qlock);
+		}
+		// has_timeout : stop alarm
+		return alloc_bool(found);
+	}
+#	endif	
 }
 
 DEFINE_PRIM(thread_create,2);
