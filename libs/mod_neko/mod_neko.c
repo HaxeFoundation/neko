45ccc8c53e1efa76d700a21af611674a251acdbc
Nicolas Cannasse <ncannasse@gmail.com>
2006-10-30 12:03:22 +0000

    fix

--- libs/mod_neko/mod_neko.c
+++ libs/mod_neko/mod_neko.c
@@ -231,6 +231,7 @@ static int neko_handler( request_rec *r ) {
 static int neko_2_0_handler( request_rec *r ) {
 	if( strcmp(r->handler,"neko-handler") != 0)
 		return DECLINED;
+	r->content_type = "text/html";
 	ap_send_http_header(r);
 	ap_rprintf(r,"You have Apache 2.0.x installed. Mod_neko2 can only run on Apache 2.2.x because of a BoehmGC issue with Apache 2.0, please upgrade to Apache 2.2.x");
 	return OK;
@@ -245,9 +246,7 @@ static int neko_init( apr_pool_t *p, apr_pool_t *plog, apr_pool_t *ptemp, server
 }
 
 static void neko_register_hooks( apr_pool_t *p ) {
-	ap_version_t v;
-	ap_get_server_revision(&v);
-	if( v.major == 2 && v.minor < 2 ) {
+	if( memcmp(ap_get_server_version(),"Apache/2.0",10) == 0 ) {
 		ap_hook_handler( neko_2_0_handler, NULL, NULL, APR_HOOK_LAST );
 		return;
 	}
