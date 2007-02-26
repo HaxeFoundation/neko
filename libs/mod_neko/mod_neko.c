c8ce22e5823ca6edc57821697eaa6ddb59b0ee7c
Nicolas Cannasse <ncannasse@gmail.com>
2007-02-26 11:09:10 +0000

    no more apache2.0 restriction.

--- libs/mod_neko/mod_neko.c
+++ libs/mod_neko/mod_neko.c
@@ -172,7 +172,7 @@ static int neko_handler_rec( request_rec *r ) {
 
 	neko_vm_redirect(vm,request_print,&ctx);
 	neko_vm_select(vm);
-	
+
 	if( ctx.main != NULL )
 		val_callEx(val_null,ctx.main,NULL,0,&exc);
 	else {
@@ -184,7 +184,7 @@ static int neko_handler_rec( request_rec *r ) {
 			*p = 0;
 		val_callEx(mload,val_field(mload,val_id("loadmodule")),args,2,&exc);
 		if( ctx.main != NULL )
-			cache_module(r,ctx.main);		
+			cache_module(r,ctx.main);
 	}
 
 	if( exc != NULL ) {
@@ -244,28 +244,15 @@ static int neko_handler( request_rec *r ) {
 
 #ifdef APACHE_2_X
 
-static int neko_2_0_handler( request_rec *r ) {
-	if( strcmp(r->handler,"neko-handler") != 0)
-		return DECLINED;
-	r->content_type = "text/html";
-	ap_send_http_header(r);
-	ap_rprintf(r,"You have Apache 2.0.x installed. Mod_neko2 can only run on Apache 2.2.x because of a BoehmGC issue with Apache 2.0, please upgrade to Apache 2.2.x");
-	return OK;
-}
-
 static int neko_init( apr_pool_t *p, apr_pool_t *plog, apr_pool_t *ptemp, server_rec *s ) {
 	cache_root = context_new();
 	use_jit = getenv("MOD_NEKO_JIT") != NULL;
 	putenv(strdup("MOD_NEKO=2"));
-	neko_global_init(&s);	
+	neko_global_init(&s);
 	return OK;
 }
 
 static void neko_register_hooks( apr_pool_t *p ) {
-	if( memcmp(ap_get_server_version(),"Apache/2.0",10) == 0 ) {
-		ap_hook_handler( neko_2_0_handler, NULL, NULL, APR_HOOK_LAST );
-		return;
-	}
 	ap_hook_post_config( neko_init, NULL, NULL, APR_HOOK_MIDDLE );
 	ap_hook_handler( neko_handler, NULL, NULL, APR_HOOK_LAST );
 };
