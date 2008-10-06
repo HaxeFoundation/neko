e44b6a1cdfdc867afa97c2853e3ab62abd5515d5
Nicolas Cannasse <ncannasse@gmail.com>
2008-10-06 07:58:48 +0000

    minor

--- libs/mod_tora/server/Admin.hx
+++ libs/mod_tora/server/Admin.hx
@@ -136,7 +136,7 @@ class Admin {
 			"Queue size : "+infos.queue,
 			"Memory : "+Std.int((mem.heap - mem.free)/1024)+" / "+Std.int(mem.heap/1024)+" KB",
 			"Total hits : "+infos.hits+" ("+fmt(infos.hits/infos.upTime)+"/sec)",
-			"Cache hits : "+cacheHits+" ("+fmt(cacheHits*100/infos.hits)+"%)",
+			"Cache hits : "+cacheHits+" ("+fmt(cacheHits*100.0/infos.hits)+"%)",
 		]);
 
 		tab("Files",function() {
