1bce771c0641b6e00fbb4c44b4548c9b7e202887
Nicolas Cannasse <ncannasse@gmail.com>
2010-01-19 15:23:04 +0000

    added unsafe port

--- libs/mod_tora/server/tora.bat
+++ libs/mod_tora/server/tora.bat
@@ -1,3 +1,3 @@
 @echo off
-neko -interp tora -config c:\progra~1\easyphp\apache\conf\httpd.conf -unsafe localhost:6667
+neko -interp tora -config c:\progra~1\easyphp\apache\conf\httpd.conf -unsafe localhost:6667 -unsafe 192.168.2.120:6667
 pause
