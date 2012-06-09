98e462a7eb7e6aa5012eea07654f600b3ec1f0a7
Nicolas Cannasse <ncannasse@gmail.com>
2012-06-09 11:14:41 +0000

    fixed warning malloc OSX

--- libs/mod_tora/protocol.c
+++ libs/mod_tora/protocol.c
@@ -15,6 +15,7 @@
 /*																			*/
 /* ************************************************************************ */
 #include <string.h>
+#include <stdlib.h>
 #include "protocol.h"
 #include "socket.h"
 
