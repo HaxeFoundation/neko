1e0cd153121282972c1bbfb35aafaaf50c318ac2
Daniel Uranga <daniel.uranga@gmail.com>
2015-07-13 02:21:38 -0300

    Dont use wildcards for Haxe Test Program imports

--- test/RunTests.hx
+++ test/RunTests.hx
@@ -1,14 +1,13 @@
 package ;
 
-import haxe.unit.*;
-import tests.*;
+import haxe.unit.TestRunner;
 
 class RunTests {
 
 	static function main() {
 		var runner = new TestRunner();
-		runner.add(new Test1());
-		runner.add(new Test2());
+		runner.add(new tests.Test1());
+		runner.add(new tests.Test2());
 		runner.run();
 		trace(runner.result.toString());
 		if (!runner.result.success) {
--- test/tests/Test1.hx
+++ test/tests/Test1.hx
@@ -1,6 +1,6 @@
 package tests;
 
-import haxe.unit.*;
+import haxe.unit.TestCase;
 
 class Test1 extends TestCase {
 
