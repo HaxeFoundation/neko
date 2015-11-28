6981ce6bdf080037efdfcaf24bf8cf8a2def7764
Andy Li <andy@onthewings.net>
2015-11-28 16:58:22 +0800

    License cleanup.
     * detail license break down in LICENSE
     * bump year to 2015.
     * libs/include/ocaml
        - copyright to Haxe Foundation instead of Nicolas
        - fix incorrect FSF address
     * libs/mysql: MIT instead of GPL

--- libs/include/ocaml/ml/mlmatch.ml
+++ libs/include/ocaml/ml/mlmatch.ml
@@ -1,6 +1,6 @@
 (*
  *  NekoML Compiler
- *  Copyright (c)2005 Nicolas Cannasse
+ *  Copyright (c)2005-2015 Haxe Foundation
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *)
 
 (* -----
