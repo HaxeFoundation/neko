33547c8f153c69585b11b92f6de80063e22025a4
Pascal Peridont <pperidont@motion-twin.com>
2016-03-11 12:49:32 +0100

    cert_load_defaults() for Mac OS

--- libs/ssl/ssl.c
+++ libs/ssl/ssl.c
@@ -15,6 +15,10 @@
 typedef int SOCKET;
 #endif
 
+#ifdef NEKO_MAC
+#include <Security/Security.h>
+#endif
+
 #define SOCKET_ERROR (-1)
 #define NRETRYS	20
 
@@ -368,7 +372,7 @@ static value cert_load_path(value path){
 }
 
 static value cert_load_defaults(){
-#ifdef NEKO_WINDOWS
+#if defined(NEKO_WINDOWS)
 	value v;
 	HCERTSTORE store;
 	PCCERT_CONTEXT cert;
@@ -383,6 +387,50 @@ static value cert_load_defaults(){
 	v = alloc_abstract(k_cert, chain);
 	val_gc(v,free_cert);
 	return v;
+#elif defined(NEKO_MAC)
+	CFMutableDictionaryRef search;
+	CFArrayRef result;
+	SecKeychainRef keychain;
+	SecCertificateRef item;
+	CFDataRef dat;
+	value v;
+	mbedtls_x509_crt *chain = NULL;
+
+	// Load keychain
+	if( SecKeychainOpen("/System/Library/Keychains/SystemRootCertificates.keychain",&keychain) != errSecSuccess )
+		return val_null;
+
+	// Search for certificates
+	search = CFDictionaryCreateMutable( NULL, 0, NULL, NULL );
+	CFDictionarySetValue( search, kSecClass, kSecClassCertificate );
+	CFDictionarySetValue( search, kSecMatchLimit, kSecMatchLimitAll );
+	CFDictionarySetValue( search, kSecReturnRef, kCFBooleanTrue );
+	CFDictionarySetValue( search, kSecMatchSearchList, CFArrayCreate(NULL, (const void **)&keychain, 1, NULL) );
+	if( SecItemCopyMatching( search, (CFTypeRef *)&result ) == errSecSuccess ){
+		CFIndex n = CFArrayGetCount( result );
+		for( CFIndex i = 0; i < n; i++ ){
+			item = (SecCertificateRef)CFArrayGetValueAtIndex( result, i );
+
+			// Get certificate in DER format
+			dat = SecCertificateCopyData( item );
+			if( dat ){
+				if( chain == NULL ){
+					chain = (mbedtls_x509_crt *)alloc(sizeof(mbedtls_x509_crt));
+					mbedtls_x509_crt_init( chain );
+				}
+				mbedtls_x509_crt_parse_der( chain, (unsigned char *)CFDataGetBytePtr(dat), CFDataGetLength(dat) );
+				CFRelease( dat );
+			}
+		}
+	}
+	CFRelease(keychain);
+	if( chain != NULL ){
+		v = alloc_abstract(k_cert, chain);
+		val_gc(v,free_cert);
+		return v;
+	}else{
+		return val_null;
+	}
 #else
 	return val_null;
 #endif
