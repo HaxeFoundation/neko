75f2fc1ba8882c67becf8dbab8a52c982227b76d
Andy Li <andy@onthewings.net>
2016-04-13 07:12:16 +0800

    [cmake] use post install script to flatten archives, remove DIST option

---
+++ cmake/post_install.cmake
@@ -0,0 +1,17 @@
+# Detect if the install is run by CPack.
+if (${CMAKE_INSTALL_PREFIX} MATCHES "/_CPack_Packages/.*/(TGZ|ZIP)/")
+
+	# Flatten the directory structure such that everything except the header files is placed in root.
+	file(GLOB bin_files ${CMAKE_INSTALL_PREFIX}/bin/*)
+	file(GLOB lib_files ${CMAKE_INSTALL_PREFIX}/lib/**/*)
+	foreach(file ${bin_files} ${lib_files})
+		get_filename_component(file_name ${file} NAME)
+		execute_process(
+			COMMAND ${CMAKE_COMMAND} -E rename
+			${file}
+			${CMAKE_INSTALL_PREFIX}/${file_name}
+		)
+	endforeach()
+	execute_process( COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_INSTALL_PREFIX}/bin)
+	execute_process( COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_INSTALL_PREFIX}/lib)
+endif()
