888a4b58e89f9ef3e665d81989cdd915206f1a9f
Andy Li <andy@onthewings.net>
2016-04-18 05:06:53 +0800

    Compile mbedtls with CMake instead of using pre-compiled lib on Windows.

--- cmake/patch_mbedtls.cmake
+++ cmake/patch_mbedtls.cmake
@@ -1,6 +1,8 @@
 # Apply config adjustments similer to Debian's
 # https://anonscm.debian.org/cgit/collab-maint/mbedtls.git/tree/debian/patches/01_config.patch
 
+set(config ${MbedTLS_source}/include/mbedtls/config.h)
+
 file(READ ${config} content)
 
 # disable support for SSL 3.0
@@ -9,12 +11,34 @@ string(REPLACE
 	"//#define MBEDTLS_SSL_PROTO_SSL3"
 	content ${content}
 )
-# enable pthread mutexes
-string(REPLACE 
-	"//#define MBEDTLS_THREADING_PTHREAD"
-	"#define MBEDTLS_THREADING_PTHREAD"
-	content ${content}
-)
+
+if (WIN32)
+	# allow alternate threading implementation
+	string(REPLACE 
+		"//#define MBEDTLS_THREADING_ALT"
+		"#define MBEDTLS_THREADING_ALT"
+		content ${content}
+	)
+	# disable the TCP/IP networking routines
+	# such that it wouldn't interfere with the #include <windows.h> in our threading_alt.h
+	string(REPLACE 
+		"#define MBEDTLS_NET_C"
+		"//#define MBEDTLS_NET_C"
+		content ${content}
+	)
+
+	file(COPY ${source}/libs/include/ssl/threading_alt.h
+		DESTINATION ${MbedTLS_source}/include/mbedtls/
+	)
+else()
+	# enable pthread mutexes
+	string(REPLACE 
+		"//#define MBEDTLS_THREADING_PTHREAD"
+		"#define MBEDTLS_THREADING_PTHREAD"
+		content ${content}
+	)
+endif()
+
 # enable the HAVEGE random generator
 string(REPLACE 
 	"//#define MBEDTLS_HAVEGE_C"
