1b0bb46cf349cc49a7e6013c15fbda12c9bfb5bd
Andy Li <andy@onthewings.net>
2016-04-26 03:05:53 +0800

    fixed source_archive_fat

--- cmake/source_archive_fat.cmake
+++ cmake/source_archive_fat.cmake
@@ -22,11 +22,19 @@ else()
 		LIST_DIRECTORIES FALSE
 		${bin_dir}/${lib_src_dir}/*
 	)
+	file(COPY ${archives} DESTINATION ${source_archive_dir}/${source_archive_fat_name_we}/${lib_src_dir})
 endif()
 
-execute_process(
-	COMMAND ${CMAKE_COMMAND} -E tar czf ${source_archive_fat_name} ${source_archive_dir}/${source_archive_fat_name_we}
-	WORKING_DIRECTORY ${source_archive_dir}
-)
+if (${source_archive_fat_name} MATCHES ^.*.zip$)
+	execute_process(
+		COMMAND ${CMAKE_COMMAND} -E tar cf ${source_archive_fat_name} ${source_archive_dir}/${source_archive_fat_name_we} --format=zip
+		WORKING_DIRECTORY ${source_archive_dir}
+	)
+else()
+	execute_process(
+		COMMAND ${CMAKE_COMMAND} -E tar czf ${source_archive_fat_name} ${source_archive_dir}/${source_archive_fat_name_we} 
+		WORKING_DIRECTORY ${source_archive_dir}
+	)
+endif()
 
 file(REMOVE_RECURSE ${source_archive_dir}/${source_archive_fat_name_we})
