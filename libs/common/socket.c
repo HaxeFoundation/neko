7dcd6df579c35a2aac9754a08554f4a80283b907
Brad Smith <brad@comstyle.com>
2026-09-18 21:36:15 -0400

    Recognize the individual BSDs
    Recognize the individual BSDs and use the new macros in the socket
    code.

--- libs/common/socket.c
+++ libs/common/socket.c
@@ -120,7 +120,8 @@ PHOST phost_resolve( const char *host ) {
 	PHOST ip = inet_addr(host);
 	if( ip == INADDR_NONE ) {
 		struct hostent *h;
-#	if defined(OS_WINDOWS) || defined(OS_MAC) || defined(OS_CYGWIN) || defined(__NetBSD__) || defined(__OpenBSD__)
+#	if defined(OS_WINDOWS) || defined(OS_MAC) || defined(OS_CYGWIN) || \
+	    defined(OS_NETBSD) || defined(OS_OPENBSD)
 		h = gethostbyname(host);
 #	else
 		struct hostent hbase;
